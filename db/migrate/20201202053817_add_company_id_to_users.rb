812d87128a4b4f2f6c5de84d8adebd578e7cded6
jlarsen-tranont <jlarsen@tranont.com>
2020-12-02 12:18:12 -0700

    fixed database

--- db/migrate/20201202053817_add_company_id_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddCompanyIdToUsers < ActiveRecord::Migration[6.0]
-  def change
-    add_column :users, :company_id, :id
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_12_02_160132) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "companies", force: :cascade do |t|
     t.string "name"
     t.integer "num_employees"
@@ -27,7 +30,21 @@ ActiveRecord::Schema.define(version: 2020_12_02_160132) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-# Could not dump table "users" because of following StandardError
-#   Unknown type 'id' for column 'company_id'
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_role"
+    t.datetime "last_time"
+    t.integer "status"
+    t.string "f_name"
+    t.string "l_name"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
 
 end
