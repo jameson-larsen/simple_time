89061147016e922c02d5b96251549f380b9517a4
jlarsen-tranont <jlarsen@tranont.com>
2020-12-02 13:05:19 -0700

    fix timezone

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 class ApplicationController < ActionController::Base
+    around_action :switch_time_zone, :if => :current_user, only: :switch_time_zone
+
+    def switch_time_zone(&block)
+        Time.use_zone(current_user.time_zone, &block)
+    end
     def after_sign_in_path_for(resource)
         dashboard_path
     end
