0060ae85b35e45510988e454ac2c4f8f8613d48a
Jameson Larsen <jamesonlarsen@Jamesons-MacBook-Pro.local>
2020-12-18 00:37:44 -0700

    removed turbolinks

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,7 +5,6 @@
 import "../stylesheets/application"
 
 require("@rails/ujs").start()
-require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 global.toastr = require("toastr")
